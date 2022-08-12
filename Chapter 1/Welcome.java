95c837b94504870a5e18ef31cab996b88115dbb2
Gr1mReap3r <111082389+Gr1mReap3r@users.noreply.github.com>
2022-08-12 15:25:21 -0400

    Code Cleanup

--- Chapter 1/Welcome.java
+++ Chapter 1/Welcome.java
@@ -4,8 +4,8 @@
 * This program will display Welcome to Java! on the console
 */
 
-public class Welcome{
-    public static void main( String[] args ){
-        System.out.println("Welcome to Java!");
+public class Welcome {
+    public static void main( String[] args ) {
+        System.out.println( "Welcome to Java!" );
     }
 }
