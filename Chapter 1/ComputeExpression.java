76c290f6eac3a24ea29066837f3170074ab9badf
Gr1mReap3r <111082389+Gr1mReap3r@users.noreply.github.com>
2022-08-15 08:30:45 -0400

    AM Coding

---
+++ Chapter 1/ComputeExpression.java
@@ -0,0 +1,18 @@
+/*
+* Programmed BY: Gr1mReap3r
+* Aug 15, 2022
+* This program will show how to do a mathematical equation and print it to the console
+*/
+
+/*
+ * Mathematical Equation
+ *  10.5 + 2 x 3
+    ____________
+      45 - 3.5
+ */
+
+ public class ComputeExpression {
+    public static void main( String[] args ) {
+        System.out.println( ( 10.5 + 2 * 3 ) / ( 45 - 3.5 ) );
+    }
+ }
---
+++ Chapter 1/WelcomeWithThreeMessages.java
@@ -0,0 +1,13 @@
+/*
+* Programmed BY: Gr1mReap3r
+* Aug 15, 2022
+* This program will display three different messages on the console
+*/
+
+public class WelcomeWithThreeMessages {
+    public static void main( String[] args ){
+        System.out.prinln( "Programming is fun!" );
+        System.out.prinln( "Fundamentals First" );
+        System.out.prinln( "Problem Driven" );
+    }
+}
