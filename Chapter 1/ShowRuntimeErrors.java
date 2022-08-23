67b2435a9974c82fe8e3ca6ab3159a8418af3ece
Gr1mReap3r <darien.booker@gdc.ga.gov>
2022-08-23 08:55:01 -0400

    Morning Work

---
+++ Chapter 1/ShowRuntimeErrors.java
@@ -0,0 +1,11 @@
+/*
+ * Programmed BY: Gr1mReap3r
+ * Aug 23, 2022
+ * This program will demonstrate runtime errors
+ */
+
+public class ShowRuntimeErrors {
+    public static void main(String[] args) {
+        System.out.println(1 / 0);
+    }
+}
---
+++ Chapter 1/ShowSyntaxErrors.java
@@ -0,0 +1,14 @@
+/*
+ * Programmed BY: Gr1mReap3r
+ * Aug 23, 2022
+ * This program will demonstrate Syntax Errors
+ */
+
+// Uncomment to see the errors "/* */" both of these
+
+/* public class ShowSyntaxErrors {
+    public static main(String[] args) {
+        System.out.println( "Welcome to Java );
+    }
+}
+*/
